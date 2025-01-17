220b8ac87605f1b6d5d61e301533c9c83206eccb
Egemen Göl <egemengol@gmail.com>
2025-01-17 16:16:06 +0300

    finalize CI?

--- index.js
+++ index.js
@@ -32,10 +32,10 @@ switch (platform) {
   case 'android':
     switch (arch) {
       case 'arm64':
-        localFileExisted = existsSync(join(__dirname, '..android-arm64.node'))
+        localFileExisted = existsSync(join(__dirname, 'zk-passport-emrtd-lib.android-arm64.node'))
         try {
           if (localFileExisted) {
-            nativeBinding = require('./..android-arm64.node')
+            nativeBinding = require('./zk-passport-emrtd-lib.android-arm64.node')
           } else {
             nativeBinding = require('zk-passport-emrtd-lib-android-arm64')
           }
@@ -44,10 +44,10 @@ switch (platform) {
         }
         break
       case 'arm':
-        localFileExisted = existsSync(join(__dirname, '..android-arm-eabi.node'))
+        localFileExisted = existsSync(join(__dirname, 'zk-passport-emrtd-lib.android-arm-eabi.node'))
         try {
           if (localFileExisted) {
-            nativeBinding = require('./..android-arm-eabi.node')
+            nativeBinding = require('./zk-passport-emrtd-lib.android-arm-eabi.node')
           } else {
             nativeBinding = require('zk-passport-emrtd-lib-android-arm-eabi')
           }
@@ -63,11 +63,11 @@ switch (platform) {
     switch (arch) {
       case 'x64':
         localFileExisted = existsSync(
-          join(__dirname, '..win32-x64-msvc.node')
+          join(__dirname, 'zk-passport-emrtd-lib.win32-x64-msvc.node')
         )
         try {
           if (localFileExisted) {
-            nativeBinding = require('./..win32-x64-msvc.node')
+            nativeBinding = require('./zk-passport-emrtd-lib.win32-x64-msvc.node')
           } else {
             nativeBinding = require('zk-passport-emrtd-lib-win32-x64-msvc')
           }
@@ -77,11 +77,11 @@ switch (platform) {
         break
       case 'ia32':
         localFileExisted = existsSync(
-          join(__dirname, '..win32-ia32-msvc.node')
+          join(__dirname, 'zk-passport-emrtd-lib.win32-ia32-msvc.node')
         )
         try {
           if (localFileExisted) {
-            nativeBinding = require('./..win32-ia32-msvc.node')
+            nativeBinding = require('./zk-passport-emrtd-lib.win32-ia32-msvc.node')
           } else {
             nativeBinding = require('zk-passport-emrtd-lib-win32-ia32-msvc')
           }
@@ -91,11 +91,11 @@ switch (platform) {
         break
       case 'arm64':
         localFileExisted = existsSync(
-          join(__dirname, '..win32-arm64-msvc.node')
+          join(__dirname, 'zk-passport-emrtd-lib.win32-arm64-msvc.node')
         )
         try {
           if (localFileExisted) {
-            nativeBinding = require('./..win32-arm64-msvc.node')
+            nativeBinding = require('./zk-passport-emrtd-lib.win32-arm64-msvc.node')
           } else {
             nativeBinding = require('zk-passport-emrtd-lib-win32-arm64-msvc')
           }
@@ -108,10 +108,10 @@ switch (platform) {
     }
     break
   case 'darwin':
-    localFileExisted = existsSync(join(__dirname, '..darwin-universal.node'))
+    localFileExisted = existsSync(join(__dirname, 'zk-passport-emrtd-lib.darwin-universal.node'))
     try {
       if (localFileExisted) {
-        nativeBinding = require('./..darwin-universal.node')
+        nativeBinding = require('./zk-passport-emrtd-lib.darwin-universal.node')
       } else {
         nativeBinding = require('zk-passport-emrtd-lib-darwin-universal')
       }
@@ -119,10 +119,10 @@ switch (platform) {
     } catch {}
     switch (arch) {
       case 'x64':
-        localFileExisted = existsSync(join(__dirname, '..darwin-x64.node'))
+        localFileExisted = existsSync(join(__dirname, 'zk-passport-emrtd-lib.darwin-x64.node'))
         try {
           if (localFileExisted) {
-            nativeBinding = require('./..darwin-x64.node')
+            nativeBinding = require('./zk-passport-emrtd-lib.darwin-x64.node')
           } else {
             nativeBinding = require('zk-passport-emrtd-lib-darwin-x64')
           }
@@ -132,11 +132,11 @@ switch (platform) {
         break
       case 'arm64':
         localFileExisted = existsSync(
-          join(__dirname, '..darwin-arm64.node')
+          join(__dirname, 'zk-passport-emrtd-lib.darwin-arm64.node')
         )
         try {
           if (localFileExisted) {
-            nativeBinding = require('./..darwin-arm64.node')
+            nativeBinding = require('./zk-passport-emrtd-lib.darwin-arm64.node')
           } else {
             nativeBinding = require('zk-passport-emrtd-lib-darwin-arm64')
           }
@@ -152,10 +152,10 @@ switch (platform) {
     if (arch !== 'x64') {
       throw new Error(`Unsupported architecture on FreeBSD: ${arch}`)
     }
-    localFileExisted = existsSync(join(__dirname, '..freebsd-x64.node'))
+    localFileExisted = existsSync(join(__dirname, 'zk-passport-emrtd-lib.freebsd-x64.node'))
     try {
       if (localFileExisted) {
-        nativeBinding = require('./..freebsd-x64.node')
+        nativeBinding = require('./zk-passport-emrtd-lib.freebsd-x64.node')
       } else {
         nativeBinding = require('zk-passport-emrtd-lib-freebsd-x64')
       }
@@ -168,11 +168,11 @@ switch (platform) {
       case 'x64':
         if (isMusl()) {
           localFileExisted = existsSync(
-            join(__dirname, '..linux-x64-musl.node')
+            join(__dirname, 'zk-passport-emrtd-lib.linux-x64-musl.node')
           )
           try {
             if (localFileExisted) {
-              nativeBinding = require('./..linux-x64-musl.node')
+              nativeBinding = require('./zk-passport-emrtd-lib.linux-x64-musl.node')
             } else {
               nativeBinding = require('zk-passport-emrtd-lib-linux-x64-musl')
             }
@@ -181,11 +181,11 @@ switch (platform) {
           }
         } else {
           localFileExisted = existsSync(
-            join(__dirname, '..linux-x64-gnu.node')
+            join(__dirname, 'zk-passport-emrtd-lib.linux-x64-gnu.node')
           )
           try {
             if (localFileExisted) {
-              nativeBinding = require('./..linux-x64-gnu.node')
+              nativeBinding = require('./zk-passport-emrtd-lib.linux-x64-gnu.node')
             } else {
               nativeBinding = require('zk-passport-emrtd-lib-linux-x64-gnu')
             }
@@ -197,11 +197,11 @@ switch (platform) {
       case 'arm64':
         if (isMusl()) {
           localFileExisted = existsSync(
-            join(__dirname, '..linux-arm64-musl.node')
+            join(__dirname, 'zk-passport-emrtd-lib.linux-arm64-musl.node')
           )
           try {
             if (localFileExisted) {
-              nativeBinding = require('./..linux-arm64-musl.node')
+              nativeBinding = require('./zk-passport-emrtd-lib.linux-arm64-musl.node')
             } else {
               nativeBinding = require('zk-passport-emrtd-lib-linux-arm64-musl')
             }
@@ -210,11 +210,11 @@ switch (platform) {
           }
         } else {
           localFileExisted = existsSync(
-            join(__dirname, '..linux-arm64-gnu.node')
+            join(__dirname, 'zk-passport-emrtd-lib.linux-arm64-gnu.node')
           )
           try {
             if (localFileExisted) {
-              nativeBinding = require('./..linux-arm64-gnu.node')
+              nativeBinding = require('./zk-passport-emrtd-lib.linux-arm64-gnu.node')
             } else {
               nativeBinding = require('zk-passport-emrtd-lib-linux-arm64-gnu')
             }
@@ -226,11 +226,11 @@ switch (platform) {
       case 'arm':
         if (isMusl()) {
           localFileExisted = existsSync(
-            join(__dirname, '..linux-arm-musleabihf.node')
+            join(__dirname, 'zk-passport-emrtd-lib.linux-arm-musleabihf.node')
           )
           try {
             if (localFileExisted) {
-              nativeBinding = require('./..linux-arm-musleabihf.node')
+              nativeBinding = require('./zk-passport-emrtd-lib.linux-arm-musleabihf.node')
             } else {
               nativeBinding = require('zk-passport-emrtd-lib-linux-arm-musleabihf')
             }
@@ -239,11 +239,11 @@ switch (platform) {
           }
         } else {
           localFileExisted = existsSync(
-            join(__dirname, '..linux-arm-gnueabihf.node')
+            join(__dirname, 'zk-passport-emrtd-lib.linux-arm-gnueabihf.node')
           )
           try {
             if (localFileExisted) {
-              nativeBinding = require('./..linux-arm-gnueabihf.node')
+              nativeBinding = require('./zk-passport-emrtd-lib.linux-arm-gnueabihf.node')
             } else {
               nativeBinding = require('zk-passport-emrtd-lib-linux-arm-gnueabihf')
             }
@@ -255,11 +255,11 @@ switch (platform) {
       case 'riscv64':
         if (isMusl()) {
           localFileExisted = existsSync(
-            join(__dirname, '..linux-riscv64-musl.node')
+            join(__dirname, 'zk-passport-emrtd-lib.linux-riscv64-musl.node')
           )
           try {
             if (localFileExisted) {
-              nativeBinding = require('./..linux-riscv64-musl.node')
+              nativeBinding = require('./zk-passport-emrtd-lib.linux-riscv64-musl.node')
             } else {
               nativeBinding = require('zk-passport-emrtd-lib-linux-riscv64-musl')
             }
@@ -268,11 +268,11 @@ switch (platform) {
           }
         } else {
           localFileExisted = existsSync(
-            join(__dirname, '..linux-riscv64-gnu.node')
+            join(__dirname, 'zk-passport-emrtd-lib.linux-riscv64-gnu.node')
           )
           try {
             if (localFileExisted) {
-              nativeBinding = require('./..linux-riscv64-gnu.node')
+              nativeBinding = require('./zk-passport-emrtd-lib.linux-riscv64-gnu.node')
             } else {
               nativeBinding = require('zk-passport-emrtd-lib-linux-riscv64-gnu')
             }
@@ -283,11 +283,11 @@ switch (platform) {
         break
       case 's390x':
         localFileExisted = existsSync(
-          join(__dirname, '..linux-s390x-gnu.node')
+          join(__dirname, 'zk-passport-emrtd-lib.linux-s390x-gnu.node')
         )
         try {
           if (localFileExisted) {
-            nativeBinding = require('./..linux-s390x-gnu.node')
+            nativeBinding = require('./zk-passport-emrtd-lib.linux-s390x-gnu.node')
           } else {
             nativeBinding = require('zk-passport-emrtd-lib-linux-s390x-gnu')
           }
